02fa163d35682e8d5ab6cd8a607730d508bbbc71
hjohns12 <hopecaneel@gmail.com>
2020-10-28 14:16:04 -0400

    add data loading functions

--- R/0-utilities.R
+++ R/0-utilities.R
@@ -1,20 +1,64 @@
-
+library(readxl)
+library(tidyverse)
+library(lubridate)
 
 # State cleaning helpers --------------------------------------------------
-
-filter_by_state <- function(x) {
-  
+read_sheets <- function(xlsx_file){
+  xlsx_file %>%
+    excel_sheets() %>%
+    set_names() %>%
+    map_df(~ read_excel(path = xlsx_file, sheet = .x, col_types = "text"), .id = 'sheet_name') %>%
+    select(sheet_name, everything())
 }
 
-filter_by_date <- function(x) {
+## Date format must be two date: c("2020-03-05", "2020-06-28")
+load_data <- function(data_path, 
+                      last_date_collected, 
+                      filter_state=NULL, 
+                      filter_facility=NULL,
+                      filter_date=NULL) {
   
+  file_name <- str_c("Covid Custody Project_", last_date_collected, ".xlsx")
+  historical_datafile <- file.path(data_path, file_name)
+  all_dat <- read_sheets(xlsx_file = historical_datafile)
+  
+  ##Convert date-type 
+  all_dat$Date <- as.Date(all_dat$Date, format = "%Y-%m-%d")
+  
+  ##No use case for filtering multi-subset of states
+  if(!is.null(filter_state)) {
+    filter1_df <- all_dat %>% 
+      dplyr::filter(State == filter_state)
+  } else {
+    filter1_df <- all_dat
+  }
+  ##Filter facility, if argument exists
+  if(!is.null(filter_facility)) {
+    filter2_df <- filter1_df %>% 
+      dplyr::filter(Facility == filter_facility)
+  } else {
+    filter2_df <- filter1_df
+  }
+  
+  ##Filter date, if argument exists
+  if(!is.null(filter_date)) {
+    filter3_df <- filter2_df %>% 
+      filter(between(FL_DATE, as.Date(filter_date[1]), as.Date(filter_date[2])))
+  } else {
+    filter3_df <- filter2_df
+  }
+  
+  return(filter3_df)
 }
+
+
 
 standardize_facility_name <- function() {
   
 }
 
 assign_prev_data <- function(x) {
+  
 }
 
 concat_by_facility <- function(x) {
