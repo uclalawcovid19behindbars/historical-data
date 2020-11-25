ed2d901232fc66d52e9b3aca774695b410602173
hjohns12 <hopecaneel@gmail.com>
2020-11-25 17:03:12 -0500

    add framework for python data merge

---
+++ py/update-data.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+
+# replace this with pulled data
+# make it a global variable
+scraped = pd.read_csv('https://github.com/uclalawcovid19behindbars/data/blob/master/Adult%20Facility%20Counts/adult_facility_covid_counts_today_latest.csv')
+
+# read in cleaned historical data
+# function with state name
+
+# bind them, making sure rows are the same
+
+# push to repo 
