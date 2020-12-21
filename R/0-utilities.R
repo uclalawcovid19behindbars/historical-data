055476338aa31c2a184cea4718c76d3ed96bcfbe
hjohns12 <hopecaneel@gmail.com>
2020-12-21 10:44:32 -0800

    fix: update NC historical data

--- R/0-utilities.R
+++ R/0-utilities.R
@@ -83,6 +83,62 @@ plot_lags <- function(dat, date, y_var, grp_var, y_lab = NULL) {
   return(plots)
 }
 
+reorder_historical_cols <- function(data, add_missing_cols=TRUE, rm_extra_cols=FALSE) {
+  historical_cols <- c("ID", "jurisdiction", "State", "Name", "Date", "source", "Residents.Confirmed",
+                    "Staff.Confirmed", "Residents.Deaths", "Staff.Deaths", "Residents.Recovered",
+                    "Staff.Recovered", "Residents.Tadmin", "Staff.Tested", "Residents.Negative",
+                    "Staff.Negative", "Residents.Pending", "Staff.Pending", "Residents.Quarantine",
+                    "Staff.Quarantine", "Residents.Active", "Residents.Tested",
+                    "Residents.Population", "Residents.Released",
+                    "Address", "Zipcode", "City", "County", "Latitude", "Longitude", "County.FIPS",
+                    "hifld_id", "TYPE", "SECURELVL", "CAPACITY", "federal_prison_type", "hifld_pop",
+                    "Website","Notes")
+  these_cols <- names(data)
+  missing_cols <- if(all(historical_cols %in% these_cols)) { NULL } else(base::setdiff(historical_cols, these_cols))
+  additional_cols <- if(all(these_cols %in% historical_cols)) { NULL } else(base::setdiff(these_cols, historical_cols))
+  
+  data <- data %>%
+    relocate(any_of(historical_cols))
+  
+  if(rm_extra_cols & (length(additional_cols) > 1 )){
+    add_out = data %>%
+      select(-all_of(additional_cols))
+  }
+  else{
+    if(length(additional_cols) > 1) {
+      warning(paste0("Input data has ", length(additional_cols),
+                     " additional columns: ", paste0(additional_cols, collapse = ", "),
+                     ". Moving these to the end of the data set."))
+      add_out <- data %>%
+        relocate(additional_cols, .after = last_col())
+    }
+    if(length(additional_cols) == 0) {
+      add_out = data
+    }
+  }
+  if(!add_missing_cols & (length(missing_cols) > 1 )) {
+    warning(paste0("Input data has ", length(missing_cols),
+                   " missing columns: ", paste0(missing_cols, collapse = ", "),
+                   ". Do you need to rename a column?"))
+    missing_out <- add_out
+  }
+  else{
+    if(length(missing_cols) > 1) {
+      warning(paste0("Input data has ", length(missing_cols),
+                     " missing columns: ", paste0(missing_cols, collapse = ", "),
+                     ". Adding these columns in as NA rows."))
+      add_out[,missing_cols] <- NA
+      missing_out <- add_out
+    }
+    if(length(missing_cols) == 0) {
+      missing_out <- add_out
+    }
+  }
+  out <- missing_out %>%
+    relocate(any_of(historical_cols))
+  return(out)
+}
+
 # State monitoring helpers --------------------------------------------------
 
 filter_statewide_NA <- function(dat, var) {
