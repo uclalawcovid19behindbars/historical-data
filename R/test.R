7a4437b14c200eadce3b1b2561386351ea5f87aa
hjohns12 <hopecaneel@gmail.com>
2020-12-23 12:21:24 -0800

    chore: update R test file

--- R/test.R
+++ R/test.R
@@ -1,5 +1,5 @@
 ## Define package list
-Packages<-c("tidyverse")
+Packages<-c("tidyverse", "devtools")
 .packages = Packages
 ## Install CRAN packages (if not already installed)
 .inst <- .packages %in% installed.packages()
@@ -7,7 +7,10 @@ if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
 ## Load packages into session 
 lapply(.packages, require, character.only=TRUE)
 
+devtools::install_github("uclalawcovid19behindbars/behindbarstools")
+
+
 a <- 1:5
-df <- tibble(a, a * 2)
+df <- tibble(a, a * 2, Sys.time())
 
 write.csv(df, "data/test.csv")
