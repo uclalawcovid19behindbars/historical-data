d15deb48d8aa6dfb485df4f79192d4dda6cc6bd3
hjohns12 <hopecaneel@gmail.com>
2021-02-12 14:56:27 -0500

    count 2020 covid deaths in FL

--- R/analysis/count_FL-deaths.R
+++ R/analysis/count_FL-deaths.R
@@ -4,7 +4,7 @@ library(behindbarstools)
 FL <- read_csv("https://raw.githubusercontent.com/uclalawcovid19behindbars/historical-data/main/data/FL-historical-data.csv")
 
 FL %>%
-  filter(Jurisdiction != "county") %>%
+  # filter(Jurisdiction != "county") %>%
   filter(Date < as.Date("2021-01-01")) %>%
   group_by(Name, Facility.ID) %>%
   arrange(desc(Date)) %>%
